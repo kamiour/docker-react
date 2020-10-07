af3098bfbb2d15b51519089e54ca1b601180ec5e
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2020-10-07 17:56:10 +0300

    edit App.js text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>I was edited</p>
+        <p>Edited text</p>
         <a
           className='App-link'
           href='https://reactjs.org'
